cd99dfb5724804f73baddc4ffcddf271b912462a
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2024-09-12 22:35:23 +0700

    Lab2 - Flatlaf

--- Lab1/src/RunClasses/Lab1_Bai2.java
+++ Lab1/src/RunClasses/Lab1_Bai2.java
@@ -2,49 +2,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package lab1;
+package RunClasses;
 
 import Utils.Tools;
-import Models.*;
+import Models.Product;
 import java.util.ArrayList;
 
 /**
  *
  * @author thnrg
  */
-public class Lab1_Bai4 {
-    private static Product tempProduct;
+public class Lab1_Bai2 {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ArrayList<Product> product = new ArrayList();
         
-        //Input
-        for(int i = 0; i < 2; i++)
+        //Output
+        for(int i = 0; i < 5; i++)
         {
-            tempProduct = new Product();
+            Product tempProduct = new Product();
             System.out.printf("Product number %d:\n", i + 1);
             tempProduct.setName(Tools.getString("Product name: "));
             tempProduct.setPrice(Tools.getDouble("Product price: "));
             product.add(tempProduct);
         }
         
-        tempProduct = new NoTaxProduct();
-        System.out.println("Product number 3:");
-        tempProduct.setName(Tools.getString("Product name: "));
-        tempProduct.setPrice(Tools.getDouble("Product price: "));
-        product.add(tempProduct);
-        
-        //Output
+        //Input
         System.out.println("Output: ");
         for(Product tempProduct: product)
         {
             System.out.printf("Product name: %s, price: %f, import tax: %f\n", tempProduct.getName(), tempProduct.getPrice(), tempProduct.getImportTax());
-            tempProduct.insert();
-            tempProduct.update();
-            tempProduct.delete();
-            tempProduct.select();
         }
     }
+    
 }
--- Lab1/src/RunClasses/Lab1_Bai4.java
+++ Lab1/src/RunClasses/Lab1_Bai4.java
@@ -2,40 +2,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package lab1;
+package RunClasses;
 
 import Utils.Tools;
-import Models.Product;
+import Models.*;
 import java.util.ArrayList;
 
 /**
  *
  * @author thnrg
  */
-public class Lab1_Bai2 {
-
+public class Lab1_Bai4 {
+    private static Product tempProduct;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ArrayList<Product> product = new ArrayList();
         
-        //Output
-        for(int i = 0; i < 5; i++)
+        //Input
+        for(int i = 0; i < 2; i++)
         {
-            Product tempProduct = new Product();
+            tempProduct = new Product();
             System.out.printf("Product number %d:\n", i + 1);
             tempProduct.setName(Tools.getString("Product name: "));
             tempProduct.setPrice(Tools.getDouble("Product price: "));
             product.add(tempProduct);
         }
         
-        //Input
+        tempProduct = new NoTaxProduct();
+        System.out.println("Product number 3:");
+        tempProduct.setName(Tools.getString("Product name: "));
+        tempProduct.setPrice(Tools.getDouble("Product price: "));
+        product.add(tempProduct);
+        
+        //Output
         System.out.println("Output: ");
         for(Product tempProduct: product)
         {
             System.out.printf("Product name: %s, price: %f, import tax: %f\n", tempProduct.getName(), tempProduct.getPrice(), tempProduct.getImportTax());
+            tempProduct.insert();
+            tempProduct.update();
+            tempProduct.delete();
+            tempProduct.select();
         }
     }
-    
 }
